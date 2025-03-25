43e66e23053d2ad2eb212bac1d46918ae99c2572
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-25 18:32:19 +0530

    feat: structured weather app

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
 import { Text, View } from "react-native";
-import Main from './Main'
 
 export default function Index() {
   return (
-    <View>
-      <Main/>
+    <View className="flex-1 justify-center items-center bg-#F0F8FF">
+      <View>
+
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }

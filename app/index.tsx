bd559aa0caf613e2e5705d1fdb388ee3bba2f7b4
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-25 18:44:17 +0530

    feat: layout of weather app

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-#F0F8FF">
-      <View>
 
+    <View className="flex-1 justify-center items-center bg-#F0F8FF w-full">
+      <View className=" justify-center items-center bg-white w-[300px] h-[350px] rounded-2xl ">
+        <Text>Current data</Text>
       </View>
-      <View>
-        
+      <View className="justify-center items-center bg-white w-[300px] h-[200px] mt-10 rounded-2xl">
+      <Text>Future data</Text>
       </View>
     </View>
+
   );
 }

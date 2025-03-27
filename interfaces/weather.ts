bf65c527a1b74f240cb6386d865be2cb23cb425f
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-27 11:54:19 +0530

    feat: interfaces ie; types of the variables

---
+++ interfaces/weather.ts
@@ -0,0 +1,61 @@
+export interface Coordinates {
+    lon: number;
+    lat: number;
+  }
+  
+  export interface WeatherCondition {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }
+  
+  export interface MainWeatherData {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level?: number;
+    grnd_level?: number;
+  }
+  
+  export interface Wind {
+    speed: number;
+    deg: number;
+    gust?: number;
+  }
+  
+  export interface Clouds {
+    all: number;
+  }
+  
+  export interface SystemData {
+    type?: number;
+    id?: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  }
+  
+  export interface WeatherData {
+    coord: Coordinates;
+    weather: WeatherCondition[];
+    base: string;
+    main: MainWeatherData;
+    visibility: number;
+    wind: Wind;
+    clouds: Clouds;
+    dt: number;
+    sys: SystemData;
+    timezone: number;
+    id: number;
+    name: string;
+    cod: number;
+  }
+  
+  export interface ApiError {
+    cod: string;
+    message: string;
+  }

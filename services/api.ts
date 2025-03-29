d1b39a7a6b2fa81d8e7d0d7b7ad51727d8dc09dc
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-29 17:34:13 +0530

    feat: api fetch

--- services/api.ts
+++ services/api.ts
@@ -2,3 +2,18 @@ import { WeatherData, ApiError } from "@/interfaces/weather";
 
 const BASE_URL =  'https://api.openweathermap.org/data/2.5';
 const API_KEY = process.env.API_KEY
+
+export const fetchWeatherbyCity = async (city: string): Promise<WeatherData> => {
+    try{
+        const response = await fetch(`${BASE_URL}/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}`);
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        console.log(error);
+        throw new Error("Failed to fetch weather data");
+    }
+}
+
+console.log(fetchWeatherbyCity('salem'));
+

c316ddec0f817f770c9136fcd344fefdbf2b6b6a
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-29 21:53:16 +0530

    fieat: api directly in the api

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import { WeatherData, ApiError } from "@/interfaces/weather";
-
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
-const API_KEY = process.env.API_KEY;
+const API_KEY = '13c4bf8c9a2e4edad2c63dd362f4ade4';
+console.log("API_KEY:", API_KEY);
 
 export const fetchWeatherbyCity = async (city: string): Promise<WeatherData> => {
     try{

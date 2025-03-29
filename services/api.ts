4f27d56716e30e668144ffc0a7e71e3de57cac4b
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-29 17:44:00 +0530

    fix: issues

--- services/api.ts
+++ services/api.ts
@@ -1,12 +1,13 @@
 import { WeatherData, ApiError } from "@/interfaces/weather";
 
-const BASE_URL =  'https://api.openweathermap.org/data/2.5';
-const API_KEY = process.env.API_KEY
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = process.env.API_KEY;
 
 export const fetchWeatherbyCity = async (city: string): Promise<WeatherData> => {
     try{
-        const response = await fetch(`${BASE_URL}/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}`);
+        const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         const data = await response.json();
+        console.log(data)
         return data;
     }
     catch(error){
@@ -15,5 +16,5 @@ export const fetchWeatherbyCity = async (city: string): Promise<WeatherData> => 
     }
 }
 
-console.log(fetchWeatherbyCity('salem'));
+(fetchWeatherbyCity('salem'));
 
